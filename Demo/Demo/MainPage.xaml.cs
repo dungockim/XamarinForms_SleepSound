ca3dd31346bf81510fba4309652d1329e9700efd
DESKTOP-HUM6943\Kim Du <dungockim@gmail.com>
2018-11-06 21:08:02 +0700

    first commit

---
+++ Demo/Demo/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace Demo
+{
+    public partial class MainPage : Xamarin.Forms.TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom)
+                .SetIsSwipePagingEnabled(false);
+        }
+    }
+}
